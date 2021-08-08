2431bf488d979afaab07f7e455d825dff045d3e0
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-08 21:31:24 +0200

    products routes and controllers added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -16,6 +17,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send(`<h1>Store API </h1><a href="/api/v1/products">products route</a>`);
 });
+
+app.use("/api/v1/products", productsRouter);
 
 //products route
 
