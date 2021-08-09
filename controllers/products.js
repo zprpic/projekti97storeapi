ee9bcfbba6bb98a5e7258c1cfcc0bb1051f10268
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-09 12:56:05 +0200

    select fields added

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,13 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("-name");
+  const products = await Product.find({}).select("name company");
   console.log(req.query);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
   if (featured) {
@@ -21,11 +21,17 @@ const getAllProducts = async (req, res) => {
   }
   //console.log(queryObject);
   let result = Product.find(queryObject);
+  //sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
+  }
+  //select
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
