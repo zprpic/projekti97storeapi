71ba601fe50d679342f1d22e68df91a8a7784290
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-09 13:47:02 +0200

    pagination added

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,12 @@
 const Product = require("../models/product");
 
+//manual setup
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select("name company");
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10)
+    .skip(5);
   console.log(req.query);
   res.status(200).json({ products, nbHits: products.length });
 };
@@ -33,6 +38,13 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
