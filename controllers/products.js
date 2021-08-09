1db73e339b8a3b54ecccd69c76671a507c6fce6d
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-09 12:39:46 +0200

    sort GET method for getAllProducts added...

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,13 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: "vase table",
-  });
+  const products = await Product.find({}).sort("-name");
+  console.log(req.query);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   const queryObject = {};
   if (featured) {
@@ -20,9 +19,16 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
-  res.status(200).json({ products, nbHits: products.length });
+  //console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
