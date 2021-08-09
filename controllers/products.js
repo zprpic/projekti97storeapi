10b70f8d53fd998499b7f93aae6cbb626750416d
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-09 15:05:58 +0200

    Numeric filters added.

--- controllers/products.js
+++ controllers/products.js
@@ -2,8 +2,8 @@ const Product = require("../models/product");
 
 //manual setup
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort("name")
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
     .select("name price")
     .limit(10)
     .skip(5);
@@ -12,7 +12,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
 
   const queryObject = {};
   if (featured) {
@@ -23,6 +23,28 @@ const getAllProducts = async (req, res) => {
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    )
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+  }
   }
   //console.log(queryObject);
   let result = Product.find(queryObject);
