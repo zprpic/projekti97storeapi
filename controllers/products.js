c61be47ac9e7db358afb3307a32bd0198de70641
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-10 12:01:51 +0200

    Accidentally added one } in app.js

--- controllers/products.js
+++ controllers/products.js
@@ -36,39 +36,38 @@ const getAllProducts = async (req, res) => {
     let filters = numericFilters.replace(
       regEx,
       (match) => `-${operatorMap[match]}-`
-    )
-    const options = ['price', 'rating']
-    filters = filters.split(',').forEach((item) => {
-      const [field, operator, value] = item.split('-')
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
       if (options.includes(field)) {
-        queryObject[field] = { [operator]: Number(value) }
+        queryObject[field] = { [operator]: Number(value) };
       }
-    })
+    });
   }
-  }
-  //console.log(queryObject);
-  let result = Product.find(queryObject);
-  //sort
-  if (sort) {
-    const sortList = sort.split(",").join(" ");
-    result = result.sort(sortList);
-  } else {
-    result = result.sort("createdAt");
-  }
-  //select
-  if (fields) {
-    const fieldsList = fields.split(",").join(" ");
-    result = result.select(fieldsList);
-  }
-  //pagination
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
-
-  result = result.skip(skip).limit(limit);
-
-  const products = await result;
-  res.status(200).json({ nbHits: products.length, products });
 };
+//console.log(queryObject);
+let result = Product.find(queryObject);
+//sort
+if (sort) {
+  const sortList = sort.split(",").join(" ");
+  result = result.sort(sortList);
+} else {
+  result = result.sort("createdAt");
+}
+//select
+if (fields) {
+  const fieldsList = fields.split(",").join(" ");
+  result = result.select(fieldsList);
+}
+//pagination
+const page = Number(req.query.page) || 1;
+const limit = Number(req.query.limit) || 10;
+const skip = (page - 1) * limit;
+
+result = result.skip(skip).limit(limit);
+
+const products = await result;
+res.status(200).json({ nbHits: products.length, products });
 
 module.exports = { getAllProductsStatic, getAllProducts };
