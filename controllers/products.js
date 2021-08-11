26a9c15a3f7054f33a04ea66b78fe4fad365069e
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-11 15:27:55 +0200

    yikes!

--- controllers/products.js
+++ controllers/products.js
@@ -45,29 +45,29 @@ const getAllProducts = async (req, res) => {
       }
     });
   }
+  //console.log(queryObject);
+  let result = Product.find(queryObject);
+  //sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  //select
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({ nbHits: products.length, products });
 };
-//console.log(queryObject);
-let result = Product.find(queryObject);
-//sort
-if (sort) {
-  const sortList = sort.split(",").join(" ");
-  result = result.sort(sortList);
-} else {
-  result = result.sort("createdAt");
-}
-//select
-if (fields) {
-  const fieldsList = fields.split(",").join(" ");
-  result = result.select(fieldsList);
-}
-//pagination
-const page = Number(req.query.page) || 1;
-const limit = Number(req.query.limit) || 10;
-const skip = (page - 1) * limit;
-
-result = result.skip(skip).limit(limit);
-
-const products = await result;
-res.status(200).json({ nbHits: products.length, products });
 
 module.exports = { getAllProductsStatic, getAllProducts };
